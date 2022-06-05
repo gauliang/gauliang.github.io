548d4791dc21ed52517d63686a0306b1b870373a
gauliang <gauliang@outlook.com>
2022-06-06 03:35:31 +0800

    deploy

--- _next/static/8HaCrOJXLh6u05bcNDpYf/_ssgManifest.js
+++
@@ -1,1 +0,0 @@
-self.__SSG_MANIFEST=new Set(["\u002Fseries","\u002F","\u002Fposts","\u002Ftags","\u002Fabout","\u002Ftags\u002F[tag]","\u002Fposts\u002F[...id]","\u002Fposts\u002Fpage\u002F[page]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
