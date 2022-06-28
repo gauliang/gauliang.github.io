e87846a3bfb2564b4d3d0c3a4828ed418c44ad08
gauliang <gauliang@outlook.com>
2022-06-28 08:11:56 +0800

    Rebuilding site Tue Jun 28 08:11:55 CST 2022

---
+++ _next/static/ymx8ekQaUzWH6QQyxjXBi/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fseries","\u002Ftags","\u002Fblogs","\u002F","\u002Fabout","\u002Fblogs\u002F[...id]","\u002Fseries\u002F[...id]","\u002Ftags\u002F[tag]","\u002Fblogs\u002Fpage\u002F[page]","\u002Fseries\u002F[series]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
